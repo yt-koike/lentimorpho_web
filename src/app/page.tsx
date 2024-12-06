bd5a43bef7198d4c4ad6aff5958cc3c0e0c04dda
yt-koike <y.koike.2001code@gmail.com>
2024-12-06 22:48:57 +0900

    add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -441,6 +441,7 @@ export default function Home() {
 
   return (
     <div>
+      <title>Lenticular Web</title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
